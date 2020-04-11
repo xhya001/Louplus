712c252b908ca6c88c771f380bca8642617bc8ef
xhya001 <1004717101@qq.com>
2020-04-11 16:29:18 +0800

    Add a  Python file

---
+++ jump7.py
@@ -0,0 +1,15 @@
+i=1
+while i<=100:
+ if i%7==0 or i//10==7 or i%10==7:
+  pass
+ else:
+  print(i)
+ i+=1
+
+print('--------------------')
+
+for i in range(101):
+ if i%7==0 or i//10==7 or i%10==7:
+  pass
+ else:
+  print(i)
